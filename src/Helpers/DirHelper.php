84f16f650f9db03b4a597e24b2b9ae3da26a3496
congnq_nexlesoft <congnq@nexlesoft.com>
2024-08-12 11:40:36 +0700

    fix bug in DirHelper.php

--- src/Helpers/DirHelper.php
+++ src/Helpers/DirHelper.php
@@ -3,39 +3,46 @@
 namespace CongnqNexlesoft\SimpleFirebaseHttpV1\Helpers;
 
 /**
- * (Last updated on July 3, 2024)
+ * (Last updated on August 12, 2024)
  * ### A Directory Helper
+ * - Required:
+ *   - File must place at /src/Helpers/DirHelper.php (Symfony project)
+ *   - File must place at /app/Helpers/DirHelper.php (Laravel project)
  */
 class DirHelper
 {
     /**
      * - Support frameworks: Symfony 4.4, Laravel 5.8 and maybe Laravel higher version
      * ---
-     * - #1.1 The paths are different between local and server (Docker) (dev,stg, prd)
+     * - [REMOVED] #1.1 The paths are different between local and server (Docker) (dev,stg, prd)
      * .e.g issue
      * - PWD (local) = (no value) ,  PWD (server) = '/srv' (no /public)
      * - DOCUMENT_ROOT (local) =  "/Users/congnqnexlesoft/Desktop/engage-api/public" (have /public)
      * - DOCUMENT_ROOT (server) = "/srv" (no /public)
      * ---
-     * - #1.2 Use a field same part between local and server, like SCRIPT_FILENAME, and replace last part '/public/index.php'
+     * - [REMOVED] #1.2 Use a field same part between local and server, like SCRIPT_FILENAME, and replace last part '/public/index.php'
      * - SCRIPT_FILENAME (local)  =  "/srv/legacy/public/index.php"
      * - SCRIPT_FILENAME (server) =  "/Users/congnqnexlesoft/Desktop/engage-api/public/index.php"
      * ---
-     * - #3 Console command
-     *    - in Symfony,
-     *        - in Mac, SCRIPT_FILENAME will return 'bin/console' , PWD = dir path
-     *        - in Docker image, same Mac
-     *    - in Laravel,
-     *        - in Mac, SCRIPT_FILENAME will return 'artisan' , PWD = dir path
-     *        - in Docker image, same Mac
+     * - [REMOVED] #3 Console command
+     *   - in Symfony,
+     *     - on Mac, SCRIPT_FILENAME will return 'bin/console' , PWD = dir path
+     *     - on Docker image, same Mac
+     *   - in Laravel,
+     *     - on Mac, SCRIPT_FILENAME will return 'artisan' , PWD = dir path
+     *     - on Docker image, same Mac
+     * - [REMOVED] #3.1 Console command when run outside project with full path, or half path,
+     *        the SCRIPT_FILENAME will return <full path>/artisan or <half path>/artisan
+     *        the SCRIPT_FILENAME will return <full path>/bin/console or <half path>/bin/console
+     * ---
+     * - #4 Final solution: Use `__FILE__` to get helper's file path and extract project path
      * ---
      * @param mixed ...$subDirOrFiles
      * @return string
      */
     public static function getWorkingDir(...$subDirOrFiles): string
-    {
-        $workingDir = in_array($_SERVER['SCRIPT_FILENAME'], ['bin/console', 'artisan']) ? $_SERVER['PWD'] // case: Console command
-            : str_replace('/public/index.php', '', $_SERVER['SCRIPT_FILENAME']); // case: PHP request
+    {                            // |-Symfony project             |-Laravel project
+        $workingDir = str_replace(['/src/Helpers/DirHelper.php', '/app/Helpers/DirHelper.php'], ['', ''], __FILE__);
         return count($subDirOrFiles) ? self::join($workingDir, ...$subDirOrFiles) : $workingDir;
     }
 
